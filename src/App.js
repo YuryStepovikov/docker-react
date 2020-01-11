7ae14cf201670226fa429adc7a7f988c0cead79f
Yury Stepovikov <stepovikov@medlinx.corp>
2020-01-11 22:59:23 +0300

    create feature branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Yep it changed again !
+          Yep it changed again in feature branch !
         </p>
         <a
           className="App-link"
